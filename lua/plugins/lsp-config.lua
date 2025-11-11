0b457bde1397db00ccedb513c216da338fb94533
GabrielDNeto <gabrielneto.contato@gmail.com>
2025-11-10 23:10:41 -0300

    feat: fix config for new nvim version

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,45 +1,54 @@
-return {
-  {
+return {{
     "williamboman/mason.nvim",
     lazy = false,
     config = function()
-      require("mason").setup()
-    end,
-  },
-  {
+        require("mason").setup()
+    end
+}, {
     "williamboman/mason-lspconfig.nvim",
     lazy = false,
     opts = {
-      auto_install = true,
+        auto_install = true
     },
     ensure_installed = {'ts_sl', 'vtsls', 'tsserver', 'lua_ls', 'tailwindcss', 'esltint'}
-  },
-  {
+}, {
     "neovim/nvim-lspconfig",
     lazy = false,
     config = function()
-      local capabilities = require('cmp_nvim_lsp').default_capabilities()
+        local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-      local lspconfig = require("lspconfig")
-      lspconfig.vtsls.setup({
-        capabilities = capabilities
-      })
+        -- Configurações por servidor
+        vim.lsp.config["vtsls"] = {
+            capabilities = capabilities
+        }
 
-      lspconfig.solargraph.setup({
-        capabilities = capabilities
-      })
-      lspconfig.html.setup({
-        capabilities = capabilities
-      })
-      lspconfig.lua_ls.setup({
-        capabilities = capabilities
-      })
+        vim.lsp.config["solargraph"] = {
+            capabilities = capabilities
+        }
 
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-      vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
-      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-    end,
-  },
-}
+        vim.lsp.config["html"] = {
+            capabilities = capabilities
+        }
+
+        vim.lsp.config["lua_ls"] = {
+            capabilities = capabilities,
+            settings = {
+                Lua = {
+                    diagnostics = {
+                        globals = {"vim"}
+                    }
+                }
+            }
+        }
+
+        -- Habilita todos
+        vim.lsp.enable({"vtsls", "solargraph", "html", "lua_ls"})
+
+        -- Keymaps
+        vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+        vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+        vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+        vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+    end
+}}
 
